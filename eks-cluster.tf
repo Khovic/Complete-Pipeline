0339fb9b79ae231629911aeb7942449de970086c
Khovic <107240579+Khovic@users.noreply.github.com>
2022-11-10 09:33:27 +0200

    added fargate to eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,3 +1,23 @@
+resource "aws_iam_role" "example" {
+  name = "eks-fargate-profile-example"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.example.name
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.30.3"
@@ -22,4 +42,15 @@ module "eks" {
       instance_types = ["t2.small"]
     }
   }
+
+  resource "aws_eks_fargate_profile" "my-fp" {
+  cluster_name           = "my-cluster"
+  fargate_profile_name   = "my-fp"
+  pod_execution_role_arn = aws_iam_role.example.arn
+  subnet_ids             = module.myapp-vpc.private_subnets
+
+  selector {
+    namespace = "fpns"
+  }
+}
 }
