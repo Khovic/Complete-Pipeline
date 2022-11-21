630a3a45985fa36398932ade9d3b972673420656
Khovic <107240579+Khovic@users.noreply.github.com>
2022-11-21 09:49:58 +0200

    updated mysql.tf

--- mysql.tf
+++ mysql.tf
@@ -8,6 +8,14 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
+provider "kubernetes" {
+  load_config_file       = "false"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
@@ -16,7 +24,7 @@ provider "helm" {
   }
 }
 
-resource "mysql" "mysql" {
+resource "helm_release" "mysql" {
   name       = "mysql"
 
   repository = "https://charts.bitnami.com/bitnami"
