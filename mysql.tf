ba51b453d8e4082c676c125cb0b228a64a11b05c
Khovic <107240579+Khovic@users.noreply.github.com>
2022-11-21 09:46:12 +0200

    added mysql.tf

---
+++ mysql.tf
@@ -0,0 +1,36 @@
+# This gives back object with certificate-authority among other attributes: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/eks_cluster#attributes-reference
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+# This gives us object with token: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/eks_cluster_auth#attributes-reference  
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+resource "mysql" "mysql" {
+  name       = "mysql"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "mysql"
+  version    = "9.4.3"
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+
+
+  values = [
+    "${file("mysql-helm-values.yaml")}"
+  ]
+
+}
