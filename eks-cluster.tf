a5854a4afda9949958130887c36ca1cb2666e51a
Khovic <107240579+Khovic@users.noreply.github.com>
2022-11-29 10:22:24 +0200

    added csi resource

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -43,6 +43,12 @@ module "eks" {
 
 }
 
+resource "aws_iam_openid_connect_provider" "openid_connect" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.cert.certificates.0.sha1_fingerprint]
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
 data "tls_certificate" "cert" {
   url = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
 }
