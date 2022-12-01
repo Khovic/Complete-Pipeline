9423c1eb1e74e03318b455ca075110f435897b74
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-01 11:05:09 +0200

    added node sg ingress

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -75,7 +75,7 @@ resource "aws_security_group_rule" "app-rule-in" {
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.eks.eks_managed_node_groups.dev.security_group_id
 }
 
@@ -84,7 +84,7 @@ resource "aws_security_group_rule" "app-rule-out" {
   from_port         = 0
   to_port           = 65535
   protocol          = "tcp"
-  cidr_blocks       = [0.0.0.0/0]
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = module.eks.eks_managed_node_groups.dev.security_group_id
 }
 
