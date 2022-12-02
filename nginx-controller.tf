029f3c10c8cc982073b2eee6f19fb5a73c89be24
Khovic <107240579+Khovic@users.noreply.github.com>
2022-12-02 09:36:05 +0200

    update nginx-controller

--- nginx-controller.tf
+++ nginx-controller.tf
@@ -1,18 +1,4 @@
 module "nginx-controller" {
   source  = "terraform-iaac/nginx-controller/helm"
-  controller_kind = "Deployment"
-  controller_daemonset_useHostPort = true
-
-  additional_set = [
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"
-      value = "nlb"
-      type  = "string"
-    },
-    {
-      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled"
-      value = "true"
-      type  = "string"
-    }
-  ]
+  version = "2.0.5"
 }
