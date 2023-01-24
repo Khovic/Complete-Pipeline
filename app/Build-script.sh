e1ee7a7ef563e83edc5676ded8ac6643c248cab7
Khovic <107240579+Khovic@users.noreply.github.com>
2023-01-24 10:34:21 +0200

    dockerfile app version

--- app/Build-script.sh
+++ app/Build-script.sh
@@ -4,5 +4,5 @@ echo ${DB_NAME}
 echo ${DB_SERVER}
 echo ${DB_PWD}
 echo ${DB_USER}
-echo ${2}
+echo $2
 docker build -t $1 . --build-arg VERSION=$2 --build-arg DB_NAME=${DB_NAME} --build-arg DB_SERVER=${DB_SERVER} --build-arg DB_PWD=${DB_PWD} --build-arg DB_USER=${DB_USER}
