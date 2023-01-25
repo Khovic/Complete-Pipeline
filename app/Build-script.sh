699b073b148a6393b01841ff31d382a303d006f1
Khovic <107240579+Khovic@users.noreply.github.com>
2023-01-25 12:42:48 +0200

    dynamic version in app-values

--- app/Build-script.sh
+++ app/Build-script.sh
@@ -5,4 +5,4 @@ echo ${DB_SERVER}
 echo ${DB_PWD}
 echo ${DB_USER}
 
-docker build -t $1 . --build-arg DB_NAME=${DB_NAME} --build-arg DB_SERVER=${DB_SERVER} --build-arg DB_PWD=${DB_PWD} --build-arg DB_USER=${DB_USER}
+docker build -t $1 . --build-arg VERSION=${VERSION} --build-arg DB_NAME=${DB_NAME} --build-arg DB_SERVER=${DB_SERVER} --build-arg DB_PWD=${DB_PWD} --build-arg DB_USER=${DB_USER}
